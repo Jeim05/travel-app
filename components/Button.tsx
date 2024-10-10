ce61fa2683342112558c17e534d910c95c234006
Jeimmy López Vega <89615089+Jeim05@users.noreply.github.com>
2024-10-10 16:04:01 -0600

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,7 +12,7 @@ const Button = ({type, title, icon, variant}:ButtonProps) => {
   return (
     <button className={`flexCenter gap-3 rounded-full border ${variant}`} type={type}>
         {icon && <Image src={icon} alt={title} width={24} height={24}/>}
-        <label className="bold whitespace-nowrap">{title}</label>
+        <label className="bold whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   )
 }
